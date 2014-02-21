4ad603e9454ddcf07f868131e183d186f2cb7016
remi <remi.oukrat@gmail.com>
2014-02-21 17:36:45 +0100

    clean code

--- NewOpenGL/triangles.cpp
+++ NewOpenGL/triangles.cpp
@@ -9,14 +9,27 @@ using namespace std;
 #include "vgl.h"
 #include "LoadShaders.h"
 
+enum VAO_IDs 
+{ 
+	TRIANGLES,
+	NUM_VAOS
+};
 
+enum Buffer_IDs 
+{ 
+	ARRAY_BUFFER,
+	NUM_BUFFERS 
+};
 
-enum VAO_IDs { Triangles, NumVAOs};
-enum Buffer_IDs { ArrayBuffer, NumBuffers };
-enum Attrib_IDs { vPosition = 0 };
-GLuint VAOs[NumVAOs];
-GLuint Buffers[NumBuffers];
+enum Attrib_IDs 
+{ 
+	V_POSITION = 0 
+};
+
+GLuint tab_vaos[NUM_VAOS];
+GLuint tab_buffers[NUM_BUFFERS];
 const GLuint NumVertices = 6;
+
 //---------------------------------------------------------------------
 //
 // init
@@ -24,8 +37,8 @@ const GLuint NumVertices = 6;
 void
 init(void)
 {
-	glGenVertexArrays(NumVAOs,VAOs);
-	glBindVertexArray(VAOs[Triangles]);
+	glGenVertexArrays(NUM_VAOS,tab_vaos);
+	glBindVertexArray(tab_vaos[TRIANGLES]);
 	GLfloat vertices[NumVertices][2] = {
 		{ -0.90, -0.90 }, // Triangle 1
 		{ 0.85, -0.90 },
@@ -34,21 +47,22 @@ init(void)
 		{ 0.90, 0.90 },
 		{ -0.85, 0.90 }
 	};
-	glGenBuffers(NumBuffers, Buffers);
-	glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+	glGenBuffers(NUM_BUFFERS, tab_buffers);
+	glBindBuffer(GL_ARRAY_BUFFER, tab_buffers[ARRAY_BUFFER]);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices),
 		vertices, GL_STATIC_DRAW);
 	ShaderInfo shaders[] = {
-		{ GL_VERTEX_SHADER, "triangles.vert" },
-		{ GL_FRAGMENT_SHADER, "triangles.frag" },
+		{ GL_VERTEX_SHADER, "TRIANGLES.vert" },
+		{ GL_FRAGMENT_SHADER, "TRIANGLES.frag" },
 		{ GL_NONE, NULL }
 	};
 	GLuint program = LoadShaders(shaders);
 	glUseProgram(program);
-	glVertexAttribPointer(vPosition, 2, GL_FLOAT,
+	glVertexAttribPointer(V_POSITION, 2, GL_FLOAT,
 		GL_FALSE, 0, BUFFER_OFFSET(0));
-	glEnableVertexAttribArray(vPosition);
+	glEnableVertexAttribArray(V_POSITION);
 }
+
 //---------------------------------------------------------------------
 //
 // display
@@ -57,10 +71,11 @@ void
 display(void)
 {
 	glClear(GL_COLOR_BUFFER_BIT);
-	glBindVertexArray(VAOs[Triangles]);
+	glBindVertexArray(tab_vaos[TRIANGLES]);
 	glDrawArrays(GL_TRIANGLES, 0, NumVertices);
 	glFlush();
 }
+
 //---------------------------------------------------------------------
 //
 // main
@@ -71,7 +86,7 @@ main(int argc, char** argv)
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGBA);
 	glutInitWindowSize(512, 512);
-	glutInitContextVersion(4, 3);
+	glutInitContextVersion(4, 0);
 	glutInitContextProfile(GLUT_CORE_PROFILE);
 	glutCreateWindow(argv[0]);
 
